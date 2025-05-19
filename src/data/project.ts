1ed7fa9a932fdd1a3c74eaaa9deccbca8442447b
miaopasixx <a13026588708@163.com>
2025-05-19 15:01:52 +0800

    feat(项目): 添加小黑屋游戏相关资源和页面
    添加小黑屋游戏的封面图片、JSON数据配置以及详细页面，支持游戏介绍、技能系统和攻略展示。调整了项目列表数据，移除示例项目，确保新游戏内容完整展示。

--- src/data/project.ts
+++ src/data/project.ts
@@ -10,33 +10,40 @@ export interface ProjectItem {
 	cover?: string[]
 }
 export const projectItems: ProjectItem[] = [
-	{
-		title: "[示例] 3D 图标资产 - 情人节",
-    title_en: "Valentine's Day 3D Assets",
-		date: "2023-10-15",
-    detail: "/detail/free-3d-valentines-assets/",
-    url: "https://web.606design.art/landings/004-free-3d-valentines-assets/",
-    cover: ['free-3d-valentines-assets/01.jpg','free-3d-valentines-assets/02.jpg','free-3d-valentines-assets/03.jpg','free-3d-valentines-assets/04.jpg',],
-		tags: ['3D', 'WEB', 'ICON']
-	},
-  {
-		title: "[示例] 待办清单网页版",
-    title_en: "Online Todo List",
-		date: "2023-10-15",
-    detail: "/detail/todo",
-    url: "https://www.ricocc.com/todo/",
-    cover: ['cover/cover-todo.jpg'],
-		tags: ['WEB', 'UI', 'TODO']
-	},
-  {
-		title: "[示例] Tink 旅行生活日记",
-    title_en: "AI Landing Page",
-		date: "2023-10-15",
-		url: "https://travellife.zeabur.app/",
-		detail: "/detail/tinklife",
-    cover: ['travel/01.jpg','travel/02.jpg','travel/03.jpg','travel/04.jpg'],
-		tags: ['WEB', 'MOBILE']
-	},
-
+		{
+			title: "Darkrom - 暗房",
+			title_en: "Darkrom - Photo Editor",
+			date: "2025-05-19",
+			detail: "/detail/darkroom",
+			url: "http://roomgame.mpsxx.top/?lang=zh_cn",
+			cover: ['cover/darkroom/dorkroom.jpg'],
+			tags: ['WEB', 'PHOTO', 'EDITOR']
+		},
+		{
+			title: "[示例] 待办清单网页版",
+			title_en: "Online Todo List",
+			date: "2023-10-15",
+			detail: "/detail/todo",
+			url: "https://www.ricocc.com/todo/",
+			cover: ['cover/cover-todo.jpg'],
+			tags: ['WEB', 'UI', 'TODO']
+		},
+	// {
+	// 	title: "[示例] 3D 图标资产 - 情人节",
+  //   title_en: "Valentine's Day 3D Assets",
+	// 	date: "2023-10-15",
+  //   detail: "/detail/free-3d-valentines-assets/",
+  //   url: "https://web.606design.art/landings/004-free-3d-valentines-assets/",
+  //   cover: ['free-3d-valentines-assets/01.jpg','free-3d-valentines-assets/02.jpg','free-3d-valentines-assets/03.jpg','free-3d-valentines-assets/04.jpg',],
+	// 	tags: ['3D', 'WEB', 'ICON']
+	// },
+  // {
+	// 	title: "[示例] Tink 旅行生活日记",
+  //   title_en: "AI Landing Page",
+	// 	date: "2023-10-15",
+	// 	url: "https://travellife.zeabur.app/",
+	// 	detail: "/detail/tinklife",
+  //   cover: ['travel/01.jpg','travel/02.jpg','travel/03.jpg','travel/04.jpg'],
+	// 	tags: ['WEB', 'MOBILE']
+	// },
 ];
-
